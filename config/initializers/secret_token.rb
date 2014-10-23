f5a6307261ffa909a03acc51537657f154764d23
lejspy <lejspy@gmail.com>
2014-10-23 15:31:05 +0200

    BD cree dans sites, et rajoute a Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sites::Application.config.secret_token = '7c3fc0b19460805fdde2c501b177cd3eaa4ce019465a28d5d323eadd87540bcb5e27c6e0b7ff3fa3f2ca956c45e18ab2de2e639e8a5adbfe2623c16cc0bda89f'
